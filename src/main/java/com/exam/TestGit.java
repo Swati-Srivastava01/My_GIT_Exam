f4c4b8306d8ec4c113a05808f702fe2590e9c710
Swati-Srivastava01 <swati.javarndcorp@gmail.com>
2022-03-15 20:39:05 +0530

    New Java file "TestGit.java" added

---
+++ src/main/java/com/exam/TestGit.java
@@ -0,0 +1,15 @@
+package com.exam.dao;
+
+public class TestGit{
+
+    public static void main(String args[]){
+		TestGit testGit = new TestGit();
+		testGit.helloWorld();
+	}
+	
+	public void helloWorld() {
+			System.out.println("Hello World");
+		}
+}
+
+	
